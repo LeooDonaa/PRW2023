9cd0769a30bfc419319e619d2cbe78f1da5f7806
LeooDonaa <leonardo.dona08@mail.com>
2023-04-25 12:11:31 -0300

    socorro

---
+++ formulario/ex6.php
@@ -0,0 +1,18 @@
+<?php
+    $nome = $_POST['nome'];
+    $nota1 = $_POST['nota1'];
+    $nota2 = $_POST['nota2'];
+    $frequencia = $_POST['frequencia'];
+    $media = ($nota1 + $nota2)/2;
+    if($frequencia>=75){
+        if($media>5){
+            echo"<h3>Aprovado</h3>";
+        }else{
+            echo"<h3 style='color: red;'>Reprovado por nota</h3>";
+        }
+    }else{
+        echo"<h3>Reprovado por frequência</h3>";
+    }
+    
+    echo"<a href='ex6.html'><button>Voltar</button></a>";
+?>
